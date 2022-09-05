ed626832966f7058505c4a080f3c985cbb373c71
EmelyAraujo <johanna261michael@gmail.com>
2022-09-05 14:31:09 -0400

    first commit

---
+++ DAL/Contexto.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tarea1.Models;
+
+namespace Tarea1.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Ocupaciones> Ocupaciones { get; set; }
+
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+        }
+
+    }
+}
---
+++ Models/Ocupaciones.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Tarea1.Models
+{
+    public class Ocupaciones
+    {
+        [Key]
+        public int OcupacionId { get; set; }
+        [Required(ErrorMessage ="La descripcion es requerida")]
+        public string? Descripcion { get; set; }
+        public float? Salario { get; set; }
+
+    }
+}
